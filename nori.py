bcc75b00fc74117e061b55f78f80aa360b938b67
dmalament <dmalament@940247f8-a3eb-dc11-81c7-0019d144f6e4>
2013-09-18 17:15:40 +0000

    no unicode_literals, it adds u to pps()
    git-svn-id: svn://fountainhead.talkpoint.com/nori/trunk@11688 940247f8-a3eb-dc11-81c7-0019d144f6e4

--- nori.py
+++ nori.py
@@ -502,7 +502,6 @@ DOCSTRING CONTENTS:
 from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
-from __future__ import unicode_literals
 
 import sys
 import atexit
