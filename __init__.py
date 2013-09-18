266b1cabdc3f13dca581fa53cd5ba68a22089bb7
dmalament <dmalament@940247f8-a3eb-dc11-81c7-0019d144f6e4>
2013-09-18 17:15:40 +0000

    add pp() everywhere on spec
    git-svn-id: svn://fountainhead.talkpoint.com/nori/trunk@11692 940247f8-a3eb-dc11-81c7-0019d144f6e4

--- __init__.py
+++ __init__.py
@@ -20,6 +20,8 @@ from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
 
+from pprint import pprint as pp  # for debugging
+
 
 ###############
 # this package
--- submodule.py
+++ submodule.py
@@ -59,6 +59,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 import sys
+from pprint import pprint as pp  # for debugging
 
 
 #########
