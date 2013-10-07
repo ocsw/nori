1d3827dd3f84ecfa3a7fb7ce00b576ea0dcfaf9e
dmalament <dmalament@940247f8-a3eb-dc11-81c7-0019d144f6e4>
2013-10-07 17:35:36 +0000

    postgresql settings stuff
    git-svn-id: svn://fountainhead.talkpoint.com/nori/trunk@11824 940247f8-a3eb-dc11-81c7-0019d144f6e4

--- dbms/postgresql.py
+++ dbms/postgresql.py
@@ -105,10 +105,8 @@ class PostgreSQL(DBMS):
     DEFAULT_REMOTE_PORT = 5432
 
     # where to look for the socket file, to set the default
-    SOCKET_SEARCH_PATH = [
-        '/var/run/mysqld/mysqld.sock',
-        '/tmp/mysql.sock',
-    ]
+    # this is a list of directories (without trailing /)
+    SOCKET_SEARCH_PATH = ['/tmp', '/var/run/postgresql', '/var/run']
 
 
     #####################################
@@ -127,9 +125,12 @@ class PostgreSQL(DBMS):
             see DBMS.create_settings()
 
         Dependencies:
-            class vars: DEFAULT_REMOTE_PORT, SOCKET_SEARCH_PATH
+            class vars: DBMS_NAME, DEFAULT_REMOTE_PORT
             instance vars: prefix, delim
-            config settings: [prefix+delim+:] port, socket_file
+            methods: settings_extra_text(),
+                     apply_config_defaults_extra()
+            config settings: [prefix+delim+:] protocol, host, port,
+                             socket_file, connect_db
             modules: core, dbms.DBMS
 
         """
@@ -140,24 +141,90 @@ class PostgreSQL(DBMS):
 
         pd = self.prefix + self.delim
 
-        # fix some defaults
-        core.config_settings[pd + 'port']['default'] = (
-            MySQL.DEFAULT_REMOTE_PORT
+        #
+        # PostgreSQL combines tcp and socket into host
+        #
+
+        del(core.config_settings[pd + 'protocol'])
+
+        core.config_settings[pd + 'host']['descr'] = (
+'''
+Hostname or socket directory for the {0} connection.
+
+If this doesn't start with '/', it will be treated as a remote hostname
+to connect to via TCP.
+
+If it does start with '/', it will be treated as the directory containing
+the Unix socket file.  The file used will be 'HOST/.s.PGSQL.PORT', where
+HOST and PORT are the given settings.
+
+Note: there is apparently no way, with {0}, to specify a
+relative directory or the full name of the socket file, or to use
+anything other than a port number as the suffix.
+
+Ignored if {1}use_ssh_tunnel is True.
+'''.format(self.__class__.DBMS_NAME)
         )
-        for f in MySQL.SOCKET_SEARCH_PATH:
-            if (core.check_file_type(f, 'MySQL socket', type_char='s',
+        # default is set in apply_config_defaults_extra()
+
+        core.config_settings[pd + 'port']['descr'] = (
+'''
+Port number for the {0} connection.
+
+Used for both TCP and socket connections; see {0}_host.
+
+Ignored if {1}use_ssh_tunnel is True.
+'''.format(self.__class__.DBMS_NAME, pd)
+        )
+        core.config_settings[pd + 'port']['default'] = (
+            self.__class__.DEFAULT_REMOTE_PORT
+        )
+
+        del(core.config_settings[pd + 'socket_file'])
+
+        # PostgreSQL requires a connection DB
+        core.config_settings[pd + 'connect_db']['descr'] = (
+'''
+Initial database for the {0} connection.
+
+{0} requires a database to connect to even for commands that
+don't use any (such as getting the list of databases).
+'''.format(self.__class__.DBMS_NAME)
+        )
+        core.config_settings[pd + 'connect_db']['default'] = 'postgres'
+
+        # fix up descriptions we replaced
+        if extra_text:
+            setting_list = ['host', 'port', 'connect_db']
+            self.settings_extra_text(setting_list, extra_text)
+
+        core.apply_config_defaults_hooks.append(
+            self.apply_config_defaults_extra
+        )
+
+
+    def apply_config_defaults_extra(self):
+        """
+        Apply configuration defaults that are last-minute/complicated.
+        Dependencies:
+            class vars: SOCKET_SEARCH_PATH
+            instance vars: prefix, delim
+            config settings: [prefix+delim+:] host, port
+            modules: core
+        """
+        pd = self.prefix + self.delim
+        # pd + 'host': first try to find a socket, then fall back to TCP
+        for d in self.__class__.SOCKET_SEARCH_PATH:
+            f = d + '/.s.PGSQL.' + core.cfg[pd + 'port']
+            if (core.check_file_type(f, 'PostgreSQL socket', type_char='s',
                                 follow_links=True, must_exist=True,
                                 use_logger=None, warn_only=True) and
-                  core.check_file_access(f, 'MySQL socket', file_rwx='rw',
-                                    use_logger=None, warn_only=True)):
-                core.config_settings[pd + 'socket_file']['default'] = f
-                break
-
-        if tunnel:
-            # make output log settings visible
-            core.config_settings_no_print_output_log(False)
-            core.config_settings['exec_path']['no_print'] = False
-            core.config_settings['print_cmds']['no_print'] = False
+                  core.check_file_access(f, 'PostgreSQL socket',
+                                         file_rwx='rw', use_logger=None,
+                                         warn_only=True)):
+                core.config_settings[pd + 'host']['default'] = f
+                return
+        core.config_settings[pd + 'host']['default'] = '127.0.0.1'
 
 
     def validate_config(self):
@@ -166,16 +233,15 @@ class PostgreSQL(DBMS):
         Only does checks that aren't done in DBMS.validate_config().
         Dependencies:
             instance vars: prefix, delim, tunnel_config
-            config settings: [prefix+delim+:] use_ssh_tunnel, protocol,
-                             host, port, socket_file
+            config settings: [prefix+delim+:] use_ssh_tunnel, host, port,
+                             connect_db
             modules: core, dbms.DBMS
         """
         pd = self.prefix + self.delim
         if not self.tunnel_config or not core.cfg[pd + 'use_ssh_tunnel']:
-            core.setting_check_list(pd + 'protocol', ['tcp', 'socket'])
-            if core.cfg[pd + 'protocol'] == 'tcp':
-                core.setting_check_is_set(pd + 'host')
-                core.setting_check_is_set(pd + 'port')
-            elif core.cfg[pd + 'protocol'] == 'socket':
-                core.setting_check_is_set(pd + 'socket_file')
+            core.setting_check_not_blank(pd + 'host')
+            if core.cfg[pd + 'host'][0] == '/':
+                core.setting_check_dir_search(pd + 'host')
+            core.setting_check_num(pd + 'port', 1, 65535)
+        core.setting_check_is_set(pd + 'connect_db')
         DBMS.validate_config(self)
