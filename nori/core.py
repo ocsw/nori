a32e331313575b2f6d58dd534a5a3a44f9b7080c
dmalament <dmalament@940247f8-a3eb-dc11-81c7-0019d144f6e4>
2014-02-20 00:01:56 +0000

    missing "system" header
    git-svn-id: svn://fountainhead.talkpoint.com/nori/trunk@13024 940247f8-a3eb-dc11-81c7-0019d144f6e4

--- nori/core.py
+++ nori/core.py
@@ -577,6 +577,10 @@ DOCSTRING CONTENTS:
 #                             MAIN IMPORTS
 ########################################################################
 
+#########
+# system
+#########
+
 from __future__ import division
 from __future__ import absolute_import
 from __future__ import print_function
